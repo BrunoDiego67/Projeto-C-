f1267dbdd91bd58c8c726a5a7241d262d332159a
Bruno Diego <103545537+BrunoDiego67@users.noreply.github.com>
2022-07-12 13:42:30 -0300

    Add files via upload

---
+++ OláMundo.cs
@@ -0,0 +1,12 @@
+namespace OlaMundo
+{
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
